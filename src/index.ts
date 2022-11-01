df7f894a3177fa545bfc5c8f33528df320d7da0a
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-11-01 20:10:58 +0300

    Set up a simple cron job every weekday at 9 AM

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import * as cron from "node-cron";
 import { Telegraf } from "telegraf";
 
 dotenv.config();
@@ -6,6 +7,12 @@ dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
 bot.start((ctx) => ctx.reply("Welcome!"));
+
+cron.schedule("0 9 * * 1-5", (now) => {
+  if (now === "manual") return; // Not sure why this is part of the type.
+  // TODO: get all users from Google Sheets, filter the ones whose docs are expiring soon
+  //     and check if a msg has already been sent and send them a message
+});
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bot.launch().then(() => console.log("Bot started"));
