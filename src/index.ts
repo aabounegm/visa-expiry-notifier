0cf9cfccacfd19c84de52c0a654b0acc9a866443
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-11-06 21:34:03 +0300

    Implement actually sending the messages

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ import { Telegraf } from "telegraf";
 // Order is necessary to load .env file before other imports
 dotenv.config();
 import { populateUsers } from "./db/utils";
+import { getPendingMesages } from "./notifier";
 
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
 // TODO: When integrating with Omnidesk, we should not intercept any messages, including /start command
-bot.start((ctx) => ctx.reply("Welcome!"));
+// bot.start((ctx) => ctx.reply("Welcome!"));
 
 // At 8:00 AM every weekday, update our database with info from Omnidesk and Google Sheets
 cron.schedule("0 8 * * 1-5", async (now) => {
@@ -19,10 +20,17 @@ cron.schedule("0 8 * * 1-5", async (now) => {
   console.log("Updated database successfully (using Google Sheets and Omnidesk)");
 });
 
-cron.schedule("0 9 * * 1-5", (now) => {
+// At 9:00 AM every weekday, send notifications to users whose visa or registration is about to expire
+cron.schedule("0 9 * * 1-5", async (now) => {
   if (now === "manual") return; // Not sure why this is part of the type.
-  // TODO: get all users from Google Sheets, filter the ones whose docs are expiring soon
-  //     and check if a msg has already been sent and send them a message
+  console.log("Checking for any expiring docs");
+  const messages = await getPendingMesages();
+  await Promise.all(
+    messages.map(({ chat_id, message }) =>
+      bot.telegram.sendMessage(chat_id, message, { parse_mode: "MarkdownV2" })
+    )
+  );
+  console.log(`Sent ${messages.length} notifications.`);
 });
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
