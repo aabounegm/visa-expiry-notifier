0b88ad2480d17af3b01a50b24ae74078ae283850
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-11-06 20:37:53 +0300

    Log some information while processing Sheet

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,5 +1,6 @@
 import { type InferCreationAttributes } from "sequelize";
 import { User } from "./user";
+import { sequelize } from "./index";
 import { getAllStudents } from "../sheets/api";
 import { fetchAllOmnideskUsers } from "../omnidesk/api";
 import { removeDuplicates } from "../omnidesk/helpers";
@@ -8,21 +9,24 @@ export const DAYS_TO_VISA_EXPIRY = 40;
 export const DAYS_TO_REGISTRATION_EXPIRY = 7;
 
 export async function populateUsers() {
-  await User.sync();
+  await sequelize.sync();
   const sheetStudents = await getAllStudents();
-  const omniUsers = removeDuplicates(await fetchAllOmnideskUsers());
+  console.log("Total students in Sheet:", sheetStudents.length);
   const sheetsUsersMap = new Map(
     sheetStudents.filter((user) => !!user.telegram).map((user) => [user.telegram, user])
   );
+  console.log("Students in Sheet with Telegram:", sheetsUsersMap.size);
+  const omniUsers = removeDuplicates(await fetchAllOmnideskUsers());
   const usersWithTg = omniUsers.filter(({ user_screen_name }) =>
     sheetsUsersMap.has(user_screen_name)
   );
+  console.log("Students in Omnidesk & Sheets:", usersWithTg.length);
   const usersToInsert: InferCreationAttributes<User>[] = usersWithTg.map((user) => {
-    const { user_screen_name, telegram_id, user_full_name } = user;
+    const { user_screen_name, telegram_id } = user;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const { registrationExpiry, visaExpiry } = sheetsUsersMap.get(user_screen_name)!;
+    const { registrationExpiry, visaExpiry, name } = sheetsUsersMap.get(user_screen_name)!;
     return {
-      fullName: user_full_name,
+      fullName: name,
       telegramUsername: user_screen_name,
       telegramChatId: Number(telegram_id),
       visaExpiration: visaExpiry,
@@ -31,5 +35,6 @@ export async function populateUsers() {
       visaLastNotified: null,
     };
   });
+  console.log("Inserting", usersToInsert.length, "users");
   await User.bulkCreate(usersToInsert, { ignoreDuplicates: true });
 }
