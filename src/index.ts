1ec448380a01e178bfe403ed780d112c85a51b9a
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-11-12 01:29:39 +0300

    Remove unused parameter

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,16 @@ import { getPendingMesages, updateLastNotified } from "./notifier";
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
 // At 8:00 AM every weekday, update our database with info from Omnidesk and Google Sheets
-cron.schedule("0 8 * * 1-5", async (now) => {
-  if (now === "manual") return; // Not sure why this is part of the type.
+cron.schedule("0 8 * * 1-5", async () => {
   await populateUsers();
   console.log("Updated database successfully (using Google Sheets and Omnidesk)");
 });
 
 // At 9:00 AM every weekday, send notifications to users whose visa or registration is about to expire
-cron.schedule("0 9 * * 1-5", async (now) => {
-  if (now === "manual") return; // Not sure why this is part of the type.
+cron.schedule("0 9 * * 1-5", async () => {
   console.log(`[${new Date().toLocaleDateString()}] Checking for any expiring docs`);
   const messages = await getPendingMesages();
+  // TODO: add some form of delay to avoid hitting rate limits
   await Promise.all(
     messages.map(async ({ chat_id, username, message, type }) => {
       console.log(`Notifying ${chat_id} (@${username}) about expiring ${type}`);
