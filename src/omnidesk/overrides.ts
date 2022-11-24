d5d609698c6c3aea8444140c96bfb7ee1c0aa11d
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-11-24 19:17:49 +0300

    Add more Omnidesk Telegram username overrides

--- src/omnidesk/overrides.ts
+++ src/omnidesk/overrides.ts
@@ -41,4 +41,24 @@ export const overrides: Override[] = [
   { user_id: 24513830, user_screen_name: "YarikaAA" }, // Arina Yartseva
   { user_id: 18051612, user_screen_name: "qt_z1k0" }, // Zeiin Kanabekov
   { user_id: 18346128, user_screen_name: "qwerty9493" }, // Davlat Magzumov
+  { user_id: 18270417, user_screen_name: "hamody28522" }, // Mohamed Hamdy
+  { user_id: 11823000, user_screen_name: "fluorescent_axolotl" }, // Du Tham Lieu
+  { user_id: 13014410, user_screen_name: "Levalih" }, // Valihan Ilyasov
+  { user_id: 18050641, user_screen_name: "Salloum_A" }, // Ali Salloum
+  { user_id: 26037016, user_screen_name: "willithemenace" }, // Wilson Ogbona
+  { user_id: 27871175, user_screen_name: "HAGMmX" }, // Naghmeh Mohammadifar
+  { user_id: 12368150, user_screen_name: "Sh1co" }, // Sherif Nafee
+  { user_id: 13322717, user_screen_name: "m_igorr" }, // Igor Mpore
+  { user_id: 15351452, user_screen_name: "nurbak_zh" }, // Nurbek Zhomartov
+  { user_id: 12556243, user_screen_name: "faragseif" }, // Seif Farag
+  { user_id: 13803858, user_screen_name: "ethanshagaei" }, // Ehsan Shaghaei
+  { user_id: 14185528, user_screen_name: "xhashxx" }, // Mohammed Shahin
+  { user_id: 12764539, user_screen_name: "KuNurdaulet" }, // Nurdaulet Kunkhozhaev
+  { user_id: 8881818, user_screen_name: "nastyaryabkova" }, // Anastasia Ryabkova
+  { user_id: 35308463, user_screen_name: "AhmadTh96" }, // Ahmad Taha
+  { user_id: 26119146, user_screen_name: "stblacq" }, // Jerome Otobong
+  { user_id: 24455535, user_screen_name: "rkharkrang" }, // Randall Kharkrang
+  { user_id: 25680626, user_screen_name: "walidkhaled98" }, // Walid Khaled
+  { user_id: 17043691, user_screen_name: "mohamed_ahmed1195" }, // Mohamed Ahmed Elsayed
+  { user_id: 7897478, user_screen_name: "khaledismaeel" }, // Khaled Ismaeel
 ].map((o) => ({ ...o, user_screen_name: o.user_screen_name.toLowerCase() }));
